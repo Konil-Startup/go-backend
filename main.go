29d5bafdb663ba51517c9f7c15e218a99e81926b
Kartochnik010 <ilyas.amantaev@gmail.com>
2023-10-10 16:59:32 +0600

    basic server

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
+var (
+	port = ":8080"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hi from server", time.Now())
+	})
+
+	log.Printf("Starting server on port %v\n", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalln(err)
+	}
 }
