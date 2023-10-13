9563ed91ffac530e75da2036c6004a56cfe1fd6f
Kartochnik010 <ilyas.amantaev@gmail.com>
2023-10-13 08:08:14 +0600

    adding config, templates cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Konil-Startup/go-backend/pkg/config"
 	"github.com/Konil-Startup/go-backend/pkg/handlers"
+	"github.com/Konil-Startup/go-backend/pkg/render"
 )
 
 var (
@@ -12,8 +14,22 @@ var (
 )
 
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	var app config.AppConfig
+
+	tCache, err := render.CacheTemplates()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.TemplateCache = tCache
+	app.UseCache = false
+	render.NewTemplates(&app)
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	http.HandleFunc("/", repo.Home)
+	http.HandleFunc("/about", repo.About)
 
 	log.Printf("Starting server on port %v\n", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,16 +1,78 @@
 package render
 
 import (
+	"bytes"
+	"html/template"
 	"log"
 	"net/http"
-	"text/template"
+	"path/filepath"
+
+	"github.com/Konil-Startup/go-backend/pkg/config"
 )
 
+var (
+	functions = template.FuncMap{}
+)
+
+var app *config.AppConfig
+
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		log.Fatalln(err)
+	var tc map[string]*template.Template
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CacheTemplates()
 	}
 
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not find template:", tmpl)
+	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func CacheTemplates() (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err := ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+			myCache[name] = ts
+		}
+
+	}
+	return myCache, nil
 }
