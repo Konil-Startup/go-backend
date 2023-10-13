cd717e0b73932cd34a98772bbd31cb7771a4baec
Kartochnik010 <ilyas.amantaev@gmail.com>
2023-10-13 15:24:12 +0600

    polluting with middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,11 +6,13 @@ import (
 	"github.com/Konil-Startup/go-backend/pkg/config"
 	"github.com/Konil-Startup/go-backend/pkg/handlers"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// mux := pat.New()
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer, NoSurf, middleware.Logger)
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Konil-Startup/go-backend
 go 1.21.0
 
 require github.com/go-chi/chi v1.5.5 // direct
+
+require github.com/justinas/nosurf v1.1.1 // indirect
