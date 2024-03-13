b17871e99d65c1de343eec44d65bb607bd5f7cf3
kartochnik010 <ilyas.amantaev@gmail.com>
2024-03-13 13:42:25 +0600

    fix job decrption, add about page

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -8,7 +8,6 @@ import (
 	"github.com/Konil-Startup/go-backend/pkg/models"
 	"github.com/Konil-Startup/go-backend/pkg/render"
 	"github.com/brianvoe/gofakeit"
-	"github.com/gorilla/mux"
 )
 
 var Repo *Repository
@@ -18,77 +17,77 @@ var stringMap = []map[string]interface{}{
 		"ImageLink":  "https://randomuser.me/api/portraits/men/9.jpg",
 		"Title":      gofakeit.Name(),
 		"Company":    gofakeit.Company(),
-		"Occupation": gofakeit.Job(),
-		"Years":      gofakeit.Price(1, 10),
-		"Price":      gofakeit.Price(0, 10000),
+		"Occupation": fmt.Sprintf("%s %s %s", gofakeit.Job().Level, gofakeit.Job().Descriptor, gofakeit.Job().Title),
+		"Years":      int(gofakeit.Price(1, 10)),
+		"Price":      int(gofakeit.Price(1000, 10000)),
 		"HelpCount":  10,
 	},
 	{
 		"ImageLink":  "https://randomuser.me/api/portraits/men/1.jpg",
 		"Title":      gofakeit.Name(),
 		"Company":    gofakeit.Company(),
-		"Occupation": gofakeit.Job(),
-		"Years":      gofakeit.Price(1, 10),
-		"Price":      gofakeit.Price(0, 10000),
+		"Occupation": fmt.Sprintf("%s %s %s", gofakeit.Job().Level, gofakeit.Job().Descriptor, gofakeit.Job().Title),
+		"Years":      int(gofakeit.Price(1, 10)),
+		"Price":      int(gofakeit.Price(1000, 10000)),
 		"HelpCount":  100,
 	},
 	{
 		"ImageLink":  "https://randomuser.me/api/portraits/men/34.jpg",
 		"Title":      gofakeit.Name(),
 		"Company":    gofakeit.Company(),
-		"Occupation": gofakeit.Job(),
-		"Years":      gofakeit.Price(1, 10),
-		"Price":      gofakeit.Price(0, 10000),
+		"Occupation": fmt.Sprintf("%s %s %s", gofakeit.Job().Level, gofakeit.Job().Descriptor, gofakeit.Job().Title),
+		"Years":      int(gofakeit.Price(1, 10)),
+		"Price":      int(gofakeit.Price(1000, 10000)),
 	},
 	{
 		"ImageLink":  "https://randomuser.me/api/portraits/men/19.jpg",
 		"Title":      gofakeit.Name(),
 		"Company":    gofakeit.Company(),
-		"Occupation": gofakeit.Job(),
-		"Years":      gofakeit.Price(1, 10),
-		"Price":      gofakeit.Price(0, 10000),
+		"Occupation": fmt.Sprintf("%s %s %s", gofakeit.Job().Level, gofakeit.Job().Descriptor, gofakeit.Job().Title),
+		"Years":      int(gofakeit.Price(1, 10)),
+		"Price":      int(gofakeit.Price(1000, 10000)),
 		"HelpCount":  99,
 	},
 	{
 		"ImageLink":  "https://randomuser.me/api/portraits/men/20.jpg",
 		"Title":      gofakeit.Name(),
 		"Company":    gofakeit.Company(),
-		"Occupation": gofakeit.Job(),
-		"Years":      gofakeit.Price(1, 10),
-		"Price":      gofakeit.Price(0, 10000),
+		"Occupation": fmt.Sprintf("%s %s %s", gofakeit.Job().Level, gofakeit.Job().Descriptor, gofakeit.Job().Title),
+		"Years":      int(gofakeit.Price(1, 10)),
+		"Price":      int(gofakeit.Price(1000, 10000)),
 	},
 	{
 		"ImageLink":  "https://randomuser.me/api/portraits/men/21.jpg",
 		"Title":      gofakeit.Name(),
 		"Company":    gofakeit.Company(),
-		"Occupation": gofakeit.Job(),
-		"Years":      gofakeit.Price(1, 10),
-		"Price":      gofakeit.Price(0, 10000),
+		"Occupation": fmt.Sprintf("%s %s %s", gofakeit.Job().Level, gofakeit.Job().Descriptor, gofakeit.Job().Title),
+		"Years":      int(gofakeit.Price(1, 10)),
+		"Price":      int(gofakeit.Price(1000, 10000)),
 	},
 	{
 		"ImageLink":  "https://randomuser.me/api/portraits/men/22.jpg",
 		"Title":      gofakeit.Name(),
 		"Company":    gofakeit.Company(),
-		"Occupation": gofakeit.Job(),
-		"Years":      gofakeit.Price(1, 10),
-		"Price":      gofakeit.Price(0, 10000),
+		"Occupation": fmt.Sprintf("%s %s %s", gofakeit.Job().Level, gofakeit.Job().Descriptor, gofakeit.Job().Title),
+		"Years":      int(gofakeit.Price(1, 10)),
+		"Price":      int(gofakeit.Price(1000, 10000)),
 	},
 	{
 		"ImageLink":  "https://randomuser.me/api/portraits/men/23.jpg",
 		"Title":      gofakeit.Name(),
 		"Company":    gofakeit.Company(),
-		"Occupation": gofakeit.Job(),
-		"Years":      gofakeit.Price(1, 10),
-		"Price":      gofakeit.Price(0, 10000),
+		"Occupation": fmt.Sprintf("%s %s %s", gofakeit.Job().Level, gofakeit.Job().Descriptor, gofakeit.Job().Title),
+		"Years":      int(gofakeit.Price(1, 10)),
+		"Price":      int(gofakeit.Price(1000, 10000)),
 		"HelpCount":  10,
 	},
 	{
 		"ImageLink":  "https://randomuser.me/api/portraits/men/24.jpg",
 		"Title":      gofakeit.Name(),
 		"Company":    gofakeit.Company(),
-		"Occupation": gofakeit.Job(),
+		"Occupation": fmt.Sprintf("%s %s %s", gofakeit.Job().Level, gofakeit.Job().Descriptor, gofakeit.Job().Title),
 		"Years":      int(gofakeit.Price(1, 10)),
-		"Price":      (gofakeit.Price(0, 10000)),
+		"Price":      int(gofakeit.Price(1000, 10000)),
 	},
 }
 
@@ -115,8 +114,14 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 
 func (m *Repository) Show(w http.ResponseWriter, r *http.Request) {
 
-	vars := mux.Vars(r)
-	fmt.Fprintln(w, vars)
-
-	render.RenderTemplate(w, "about.page.tmpl", nil)
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{Data: []map[string]interface{}{
+		{
+			"ImageLink": r.FormValue("image"),
+			"Title":     r.FormValue("title"),
+			"Com":       r.FormValue("com"),
+			"Job":       r.FormValue("job"),
+			"Years":     r.FormValue("years"),
+			"Price":     r.FormValue("price"),
+		},
+	}})
 }
